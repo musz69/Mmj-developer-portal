8acd57ed953bd0584a22e3cf36d5499697f665fe
musz69 <musran69plu@gmail.com>
2026-09-23 06:05:04 +0800

    Delete page.tsx

--- web/app/(unauthorized)/unauthorized/page.tsx
+++
@@ -1,9 +0,0 @@
-import { generateMetaTitle } from "@/lib/genarate-title";
-import { UnauthorizedPage } from "@/scenes/Unauthorized/page";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: generateMetaTitle({ left: "Unauthorized" }),
-};
-
-export default UnauthorizedPage;
